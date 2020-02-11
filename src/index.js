9b8f7d5f5585177e8ac4d6722517d688444adeb8
Aresky Berkane <areskyberkane@gmail.com>
2020-02-11 20:32:45 +0000

    Add firebase configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA6xDXPyDBrNQ4kDdp-6dP9w5QNLFh18SQ",
+    authDomain: "pi-4a942.firebaseapp.com",
+    databaseURL: "https://pi-4a942.firebaseio.com",
+    projectId: "pi-4a942",
+    storageBucket: "pi-4a942.appspot.com",
+    messagingSenderId: "739914753383",
+    appId: "1:739914753383:web:5f876b9aeae3eb58aa7bc2",
+    measurementId: "G-4PWEMLRYQ9"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
