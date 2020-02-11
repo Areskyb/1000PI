04d01019748253f9f231fef589a8631644133965
Aresky Berkane <areskyberkane@gmail.com>
2020-02-11 20:40:21 +0000

    add folder structure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-             {process.env.REACT_APP_API_KEY}
+          
         </p>
         <a
           className="App-link"
