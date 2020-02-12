f834e0be466c79e58cb3343fd61cc62f623d1bb1
Aresky Berkane <areskyberkane@gmail.com>
2020-02-12 10:34:06 +0000

    added user name data  in logged section

--- src/shared/NavBar/Logged.js
+++ src/shared/NavBar/Logged.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Avatar from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
+import * as firebase from 'firebase'
+
 
 function Logged(){
+    const [userName,setUserName] = useState(null);
+    const [userPhotoURL,setUserPhotoURL] = useState(null);
+
+
+    function userData(){
+        firebase.auth().onAuthStateChanged((user) => {
+           setUserName(user.displayName);
+           setUserPhotoURL(user.photoURL);
+        })
+    }
+
+    useEffect(() => {
+        userData()
+    }, [userName,userPhotoURL])
     return(
-        <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        <>
+        <Typography variant= "h6" style={{marginRight:10}}>{userName}</Typography>
+        <Avatar alt="Cindy Baker" src={userPhotoURL} />
+        </>
     )
 }
 
