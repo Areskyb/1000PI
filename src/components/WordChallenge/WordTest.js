223593b4612f0e4c2327c723f75f722c24e7d928
Aresky Berkane <areskyberkane@gmail.com>
2020-02-15 21:22:23 +0000

    created component

---
+++ src/components/WordChallenge/WordTest.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import { Button } from '@material-ui/core'
+
+function WordTest({wordList}){
+        const [inTest, setInTest] = useState(false)
+
+        if(inTest){
+            return(
+                <h3>in test</h3>
+            )
+        }else{
+            return(
+                <Button onClick={e => setInTest(true)}>Make Test</Button>
+
+            )
+        }
+}
+
+export default WordTest
